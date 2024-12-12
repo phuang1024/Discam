d7c0c04d7b749f9002d3e8f7f0a8ee69c56a9dd8
Patrick Huang <huangpatrick16777216@gmail.com>
2024-12-11 19:36:35 -0600

    Finish dataset.

---
+++ src/constants.py
@@ -0,0 +1,3 @@
+# See dataset
+TRANSL_FAC = 200
+SCALE_FAC = 400
--- src/dataset.py
+++ src/dataset.py
@@ -1,37 +1,52 @@
 """
-Handles processing training data.
-Includes extracting video frames and running the transform solver.
+Dataset of images and labels.
 """
 
 from pathlib import Path
 
 import torch
+import torchvision
 from torch.utils.data import Dataset
 
+from constants import *
 from video_reader import *
 
 
 class DiscamDataset(Dataset):
-    def __init__(self, data_dir, keyframe: int):
-        self.data_dir = Path(data_dir)
-        self.keyframe = keyframe
+    """
+    Modifications to label:
 
-        # Map of file name to number of samples available.
-        # Number of samples is num_frames - keyframe_interval.
-        self.samples = {}
+    Label is translation and scale.
+    Translation is usually on the order of 1e-3
+        thus, we multiply it by a large number.
+    Scale is roughly 1 +- 1e-3
+        thus, we subtract 1 and multiply by a large number.
+    """
+
+    def __init__(self, data_dir, transl_fac=TRANSL_FAC, scale_fac=SCALE_FAC):
+        self.data_dir = Path(data_dir)
+        self.transl_fac = transl_fac
+        self.scale_fac = scale_fac
+
+        self.length = 0
+
         for file in self.data_dir.iterdir():
-            length = get_video_length(str(file))
-            self.samples[file] = length - keyframe
+            if file.stem.isdigit():
+                self.length = max(self.length, int(file.stem) + 1)
 
     def __len__(self):
-        return sum(self.samples.values())
+        return self.length
 
     def __getitem__(self, idx):
-        # Convert to file name and sample index.
-        for file, num_samples in self.samples.items():
-            if idx < num_samples:
-                break
-            idx -= num_samples
-        else:
-            raise IndexError("Invalid index")
+        img_path = self.data_dir / f"{idx}.jpg"
+        img = torchvision.io.read_image(str(img_path)).float() / 255
 
+        label_path = self.data_dir / f"{idx}.txt"
+        with open(label_path) as f:
+            label = f.read().split()
+        tx, ty, scale = map(float, label)
+        tx *= self.transl_fac
+        ty *= self.transl_fac
+        scale = (scale - 1) * self.scale_fac
+
+        return img, torch.tensor([tx, ty, scale])
