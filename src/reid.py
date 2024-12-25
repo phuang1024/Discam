da68729bf952802be3b1bfd6dcce35b61d2be8c8
Patrick Huang <huangpatrick16777216@gmail.com>
2024-12-24 22:11:57 -0800

    Test ReID.

--- src/reid.py
+++ src/reid.py
@@ -8,7 +8,8 @@ from clip_reid.model.make_model_clipreid import make_model
 
 cfg.merge_from_file("reid_config.yml")
 cfg.freeze()
-print(cfg)
 
-model = make_model(cfg, num_class=751, camera_num=6, view_num=1)
-model.load_param("clip_reid.pt")
+reid_model = make_model(cfg, num_class=751, camera_num=6, view_num=1)
+reid_model.load_param("clip_reid.pt")
+reid_model.to("cuda")
+reid_model.eval()
---
+++ src/test_reid.py
@@ -0,0 +1,69 @@
+"""
+Stream a video, and track a single person.
+"""
+
+import argparse
+import random
+
+import cv2
+import torch
+
+from features import run_yolo
+from reid import reid_model
+
+
+def prepare_reid_input(frame, preds):
+    inputs = []
+    for i in range(len(preds.cls)):
+        x1, y1, x2, y2 = map(int, preds.xyxy[i])
+        person = frame[y1:y2, x1:x2]
+        person = cv2.resize(person, (128, 256))
+        person = torch.tensor(person, dtype=torch.float32)
+        person = person.permute(2, 0, 1) / 128.0 - 1.0
+        inputs.append(person)
+
+    return torch.stack(inputs).to("cuda")
+
+
+@torch.no_grad()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--video", required=True)
+    parser.add_argument("--interval", type=int, default=1)
+    args = parser.parse_args()
+
+    video = cv2.VideoCapture(args.video)
+
+    target_embed = None
+    i = 0
+    while True:
+        for _ in range(args.interval):
+            ret, frame = video.read()
+            if not ret:
+                return
+
+        preds = run_yolo(frame)
+        preds = preds[0].boxes
+
+        persons = prepare_reid_input(frame, preds)
+        results = reid_model(persons)
+
+        if i % 100 == 0:
+            res_i = random.randint(0, len(results) - 1)
+            target_embed = results[res_i].unsqueeze(0)
+            print("Recompute target")
+
+        else:
+            dists = torch.nn.functional.cosine_similarity(target_embed, results)
+            best_i = torch.argmax(dists).item()
+
+            x1, y1, x2, y2 = map(int, preds.xyxy[best_i])
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.imshow("frame", frame)
+            cv2.waitKey(10)
+
+        i += 1
+
+
+if __name__ == "__main__":
+    main()
