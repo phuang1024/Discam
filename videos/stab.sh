c13892fa6862acc284a5959fe819b2944e50a14a
Patrick Huang <huangpatrick16777216@gmail.com>
2025-08-21 16:22:36 -0500

    Made symlink.

--- videos/stab.sh
+++
@@ -1,2 +0,0 @@
-ffmpeg -i upres.mp4 -vf vidstabdetect=shakiness=7 -f null -
-ffmpeg -i upres.mp4 -vf vidstabtransform=smoothing=30:zoom=5:input="transforms.trf" stab.mp4
--- videos/upres.sh
+++
@@ -1,1 +0,0 @@
-video2x -i short.mp4 -o upres.mp4 -p realesrgan -s 2 -m 2
