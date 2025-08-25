24aaab96120dc7a6cba5b84dad90991b45d43fb4
Patrick Huang <huangpatrick16777216@gmail.com>
2025-08-25 17:20:38 -0500

    Rename.

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,14 @@
+"""
+Global constants, mainly for training.
+"""
+
 VIDEO_RES = (1920, 1080)
 MODEL_INPUT_RES = (640, 360)
 
+# Pixels per step.
 AGENT_VELOCITY = 10
+
+SIM_STEPS = 100
+SIM_FRAME_SKIP = 3
+SIMS_PER_EPOCH = 100
+EPOCHS_PER_SESSION = 10
