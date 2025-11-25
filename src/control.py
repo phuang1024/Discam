6d570058b7a0613e99310d8b152ffbc37cbbd954
Patrick Huang <huangpatrick16777216@gmail.com>
2025-11-24 19:04:16 -0600

    Fix control bugs.

--- src/control.py
+++ src/control.py
@@ -9,31 +9,44 @@ import serial
 
 class PTZControl:
     def __init__(self):
-        self.port = serial.Serial("/dev/ttyUSB1")
+        self.port = serial.Serial("/dev/ttyUSB0")
         self.addr = 1
 
-    def set_velocity(self, pan: float, tilt: float, zoom: float):
+    def set_pt(self, pan: int, tilt: int):
         """
-        Set PTZ velocity.
+        Set pan and tilt velocity.
 
-        pan, tilt, zoom are floats in [-1, 1]. +-1 corresponds to max velocity.
-
-        Positive zoom is zoom in.
+        pan, tilt: -1, 0, or 1.
         """
-        # Calculate speeds.
-        pan_spd = min(int(abs(pan) * 0x3F), 0x3F)
-        tilt_spd = min(int(abs(tilt) * 0x3F), 0x3F)
-        zoom_spd = min(int(abs(zoom) * 0x33), 0x33)
-
-        # Set zoom speed.
-        #self.send_command(0, 0x25, 0, zoom_spd)
-
-        # Send PTZ command.
         byte4 = 0
-        byte4 |= 32 if zoom > 0 else 64
-        byte4 |= 16 if tilt > 0 else 8
-        byte4 |= 2 if pan > 0 else 4
-        self.send_command(0, byte4, pan_spd, tilt_spd)
+        if tilt > 0:
+            byte4 |= 16
+        elif tilt < 0:
+            byte4 |= 8
+        if pan > 0:
+            byte4 |= 2
+        elif pan < 0:
+            byte4 |= 4
+        self.send_command(0, byte4, 0x10, 0x10)
+
+    def set_zoom(self, zoom: int):
+        """
+        Set zoom velocity.
+
+        zoom: -1, 0, or 1.
+        """
+        byte4 = 0
+        if zoom > 0:
+            byte4 |= 0x20
+        elif zoom < 0:
+            byte4 |= 0x40
+        self.send_command(0, byte4, 0, 0)
+
+    def stop(self):
+        """
+        Stop all motion.
+        """
+        self.send_command(0, 0, 0, 0)
 
     def send_command(self, byte3, byte4, byte5, byte6):
         """
@@ -41,6 +54,7 @@ class PTZControl:
         """
         command = [0xFF, self.addr, byte3, byte4, byte5, byte6]
         command.append(sum(command[1:]) % 256)
+        print(command)
         command = bytes(command)
         self.port.write(command)
         self.port.flush()
@@ -48,8 +62,16 @@ class PTZControl:
 
 if __name__ == "__main__":
     ptz = PTZControl()
-    ptz.set_velocity(0.3, 0, 0)
-    time.sleep(1)
-    ptz.set_velocity(-0.3, 0, 0)
-    time.sleep(1)
-    ptz.set_velocity(0, 0, 0)
+    for i in range(10):
+        ptz.set_pt(1, 0)
+        time.sleep(1)
+        ptz.set_pt(-1, 0)
+        time.sleep(1)
+        ptz.stop()
+
+        ptz.set_zoom(1)
+        time.sleep(1)
+        ptz.set_zoom(-1)
+        time.sleep(1)
+        ptz.stop()
+        break
