0ec28af9f23e62e7fbb6c0020c490a12d44ae38e
Patrick Huang <huangpatrick16777216@gmail.com>
2025-11-27 12:16:07 -0600

    Remove old.

--- deploy/main.py
+++ deploy/main.py
@@ -7,17 +7,7 @@ Threading:
 - Read camera thread reads camera frames into a queue, maintaining time accuracy.
 - PTZ control thread continuously outputs PTZ commands to camera.
   Periodically spawns a transient inference thread.
-- Inference thread runs neural network on latest frame, and reports result to PTZ control thread.
-  Fetches latest frame from a shared frame buffer with main thread.
 """
-
-import os
-import sys
-ROOT = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(
-    os.path.dirname(ROOT),
-    "train",
-))
 
 import argparse
 import time
@@ -26,14 +16,13 @@ from pathlib import Path
 from threading import Thread
 
 from constants2 import *
-from ptz import ptz_control_thread, nn_inference_thread
+from ptz import ptz_control_thread
 from recorder import camera_read_thread, video_write_thread
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("output", type=Path)
-    parser.add_argument("--model", type=Path)
     args = parser.parse_args()
 
     # Make timestamped output directory
@@ -49,13 +38,11 @@ def main():
     camera_read_t = Thread(target=camera_read_thread, args=(state,))
     video_write_t = Thread(target=video_write_thread, args=(state, out_dir,))
     ptz_control_t = Thread(target=ptz_control_thread, args=(state,))
-    nn_inference_t = Thread(target=nn_inference_thread, args=(state, args.model,))
 
     threads = (
         camera_read_t,
         video_write_t,
         ptz_control_t,
-        nn_inference_t,
     )
     for t in threads:
         t.start()
--- deploy/ptz.py
+++ deploy/ptz.py
@@ -3,14 +3,9 @@ PTZ control and NN inference.
 """
 
 import time
-from pathlib import Path
 
-import cv2
 import numpy as np
-import torch
 
-from model import DiscamModel
-from constants import DEVICE, MODEL_INPUT_RES
 from constants2 import *
 
 # DOFs: Pan+, Pan-, Tilt+, Tilt-, Zoom+, Zoom-
@@ -25,31 +20,6 @@ DOF_ARRAY = np.array([
 ])
 
 
-def compute_ptz(nn_output):
-    """
-    Compute PTZ commands from a queue of NN output.
-
-    Control algorithm principles:
-    - Zoom out rapidly.
-    - Don't zoom in too soon.
-    - Don't PTZ constantly.
-
-    Control algorithm:
-    - Separate movement into 6DOF: Positive and negative for each of PTZ.
-    - Compute raw command for each DOF as `raw = sum(output_to_cmd(x))`
-        where each `x` is an element in nn_output,
-        and `output_to_cmd` is a function that maps the edge weights to a
-            scalar magnitude regarding the current DOF.
-    - Apply a subtractive threshold. I.e. `cmd = max(0, raw - threshold)`.
-        The threshold depends on which DOF.
-        E.g. zoom out has a low threshold, while zoom in has a high threshold.
-    - Combine the positive and negatives into 3 DOFs, and apply an EMA to each.
-    """
-    # TODO
-    nn_output = np.array(nn_output)
-    raw_cmds = DOF_ARRAY @ nn_output
-
-
 def ptz_control_thread(state: ThreadState):
     while state.run:
         # TODO ptz algorithm
@@ -57,26 +27,3 @@ def ptz_control_thread(state: ThreadState):
         time.sleep(PTZ_INTERVAL)
 
 
-@torch.no_grad()
-def nn_inference_thread(state: ThreadState, model_path: Path):
-    model = DiscamModel().to(DEVICE)
-    model.load_state_dict(torch.load(model_path, map_location=DEVICE))
-    model.eval()
-
-    while state.run:
-        if len(state.frameq) == 0:
-            time.sleep(0.01)
-            continue
-
-        frame = state.frameq[-1]
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = cv2.resize(frame, MODEL_INPUT_RES)
-        frame = frame.astype("float32") / 255.0
-        frame = torch.from_numpy(frame)
-        frame = frame.permute(2, 0, 1).unsqueeze(0).to(DEVICE)
-        output = model(frame)
-
-        output = output.cpu().squeeze(0).numpy()
-        state.nn_output.append(output)
-
-        time.sleep(NN_INTERVAL)
