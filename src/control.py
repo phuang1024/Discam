a85af9644cb81ef8b7ff8f1457c525ea1886ba6b
Patrick Huang <huangpatrick16777216@gmail.com>
2026-01-21 10:42:47 -0600

    Compute median box.

--- src/control.py
+++ src/control.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 
 import cv2
 import numpy as np
+import torch
 from ultralytics import YOLO
 
 from constants import *
@@ -21,45 +22,58 @@ class ResultsAverage:
     """
     Queues to handle moving window average of bbox results.
     """
-    # Number of bboxes.
-    n: deque
-    x_moment: deque
-    y_moment: deque
-    size_moment: deque
+    # Center X coords of each bbox.
+    xs: deque
+    ys: deque
+    sizes: deque
 
     def __init__(self, max_len):
-        self.n = deque(maxlen=max_len)
-        self.x_moment = deque(maxlen=max_len)
-        self.y_moment = deque(maxlen=max_len)
-        self.size_moment = deque(maxlen=max_len)
-
-    def get_avg(self):
-        total_n = sum(self.n)
-        avg = (
-            sum(self.x_moment) / total_n,
-            sum(self.y_moment) / total_n,
-            sum(self.size_moment) / total_n,
-        )
-        avg = list(map(float, avg))
-        return avg
+        self.xs = deque(maxlen=max_len)
+        self.ys = deque(maxlen=max_len)
+        self.sizes = deque(maxlen=max_len)
 
     def update(self, detects):
         """
         detects: Return from detect_persons().
         """
-        x_moment = 0
-        y_moment = 0
-        size_moment = 0
+        xs = []
+        ys = []
+        sizes = []
         for box in detects:
             x1, y1, x2, y2 = box.xyxy[0]
-            x_moment += (x1 + x2) / 2
-            y_moment += (y1 + y2) / 2
-            size_moment += y2 - y1
+            xs.append((x1 + x2) / 2)
+            ys.append((y1 + y2) / 2)
+            sizes.append(y2 - y1)
 
-        self.n.append(len(detects))
-        self.x_moment.append(x_moment)
-        self.y_moment.append(y_moment)
-        self.size_moment.append(size_moment)
+        self.xs.append(xs)
+        self.ys.append(ys)
+        self.sizes.append(sizes)
+
+    def get_avg(self):
+        """
+        If no bboxes in the entire queue, returns None.
+        return {
+            n: Average bboxes per frame.
+            median_box: A new xyxy bounding box, using the 25 and 75 percentiles of
+                the centers of all detection bboxes.
+        }
+        """
+        total_n = sum(len(x) for x in self.xs)
+        if total_n == 0:
+            return None
+
+        xs = np.concatenate(self.xs)
+        ys = np.concatenate(self.ys)
+        x_min = np.quantile(xs, 0.25).item()
+        x_max = np.quantile(xs, 0.75).item()
+        y_min = np.quantile(ys, 0.25).item()
+        y_max = np.quantile(ys, 0.75).item()
+        median_box = [x_min, y_min, x_max, y_max]
+
+        return {
+            "n": total_n / len(self.xs),
+            "median_box": median_box,
+        }
 
 
 def detect_persons(frame):
@@ -87,7 +101,9 @@ def annotate_frame(detects, avg, frame):
         x1, y1, x2, y2 = map(int, box.xyxy[0])
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    cv2.circle(frame, (int(avg[0]), int(avg[1])), 5, (255, 0, 0), -1)
+    if avg is not None:
+        x1, y1, x2, y2 = map(int, avg["median_box"])
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
 
 def control_thread(state: ThreadState, log_dir):
