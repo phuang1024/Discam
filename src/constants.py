53a49188923aadc339ce80075765d4765262f61d
Patrick Huang <huangpatrick16777216@gmail.com>
2026-01-21 10:07:15 -0600

    Moving average.

--- src/constants.py
+++ src/constants.py
@@ -27,11 +27,13 @@ RECORD_CHUNK_SIZE = 5 * 60 * 24
 
 # Path to camera.
 #CAMERA_PATH = "/dev/video2"
-CAMERA_PATH = "../data/videos/BoomNov10_part.mkv"
+CAMERA_PATH = "../data/videos/RiseNotre_part.mp4"
 # If reading from a file, set this nonzero.
 CAM_READ_DELAY = 1 / FPS
 
 # FPS to run PTZ control algorithm.
 CTRL_FPS = 3
+# For all tracking parameters (e.g. bbox size, pos), average over this many control frames.
+CTRL_AVG_WINDOW = 10
 # Detection confidence threshold.
-CONF_THRES = 0.3
+CONF_THRES = 0.1
--- src/control.py
+++ src/control.py
@@ -3,6 +3,8 @@ PTZ control thread.
 """
 
 import time
+from collections import deque
+from dataclasses import dataclass
 
 import cv2
 import numpy as np
@@ -11,6 +13,50 @@ from ultralytics import YOLO
 from constants import *
 
 yolo = YOLO("yolo26n.pt")
+
+
+@dataclass
+class ResultsAverage:
+    """
+    Queues to handle moving window average of bbox results.
+    """
+    # Number of bboxes.
+    n: deque
+    x_moment: deque
+    y_moment: deque
+    size_moment: deque
+
+    def __init__(self, max_len):
+        self.n = deque(maxlen=max_len)
+        self.x_moment = deque(maxlen=max_len)
+        self.y_moment = deque(maxlen=max_len)
+        self.size_moment = deque(maxlen=max_len)
+
+    def get_avg(self):
+        total_n = sum(self.n)
+        return (
+            sum(self.x_moment) / total_n,
+            sum(self.y_moment) / total_n,
+            sum(self.size_moment) / total_n,
+        )
+
+    def update_new_results(self, results):
+        """
+        results: Return from detect_persons().
+        """
+        x_moment = 0
+        y_moment = 0
+        size_moment = 0
+        for box in results:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x_moment += (x1 + x2) / 2
+            y_moment += (y1 + y2) / 2
+            size_moment += y2 - y1
+
+        self.n.append(len(results))
+        self.x_moment.append(x_moment)
+        self.y_moment.append(y_moment)
+        self.size_moment.append(size_moment)
 
 
 def detect_persons(frame):
@@ -29,7 +75,7 @@ def detect_persons(frame):
     return boxes
 
 
-def annotate_frame(results, frame):
+def annotate_frame(results, avg, frame):
     for box in results:
         cls_id = int(box.cls[0])
         if cls_id != 0:
@@ -38,10 +84,14 @@ def annotate_frame(results, frame):
         x1, y1, x2, y2 = map(int, box.xyxy[0])
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
+    cv2.circle(frame, (int(avg[0]), int(avg[1])), 5, (255, 0, 0), -1)
+
 
 def control_thread(state: ThreadState):
     # Current PTZ.
     curr_pos = np.array([0, 0, 0], dtype=int)
+
+    avg_results = ResultsAverage(CTRL_AVG_WINDOW)
 
     while state.run:
         time.sleep(1 / CTRL_FPS)
@@ -50,8 +100,10 @@ def control_thread(state: ThreadState):
         frame = state.frameq[-1].copy()
 
         results = detect_persons(frame)
+        avg_results.update_new_results(results)
+        avg = avg_results.get_avg()
 
-        annotate_frame(results, frame)
+        annotate_frame(results, avg, frame)
         cv2.imshow("a", frame)
         cv2.waitKey(1)
 
