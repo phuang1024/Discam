98b994d7baeb85d08e1e75573f76c2c98269058a
Patrick Huang <huangpatrick16777216@gmail.com>
2026-03-10 12:27:11 -0700

    Comment.

--- timestamp/model.py
+++ timestamp/model.py
@@ -3,16 +3,22 @@
 """
 
 import torch
-import torchvision
 
 
 def create_model():
+    """
+    Last block unfrozen.
+    """
     model = torch.hub.load("facebookresearch/pytorchvideo", "slow_r50", pretrained=True)
     model.blocks[-1].proj = torch.nn.Linear(2048, 1)
 
     for param in model.parameters():
         param.requires_grad = False
-    for param in model.blocks[-1].proj.parameters():
+    """
+    for param in model.blocks[-2].parameters():
+        param.requires_grad = True
+    """
+    for param in model.blocks[-1].parameters():
         param.requires_grad = True
 
     return model
