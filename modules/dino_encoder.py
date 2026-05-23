d2d8707cd40138335b4f2f8c3f1d486a3603cd16
Patrick Huang <huangpatrick16777216@gmail.com>
2026-05-23 12:27:05 -0500

    Perspective warp.

--- modules/dino_encoder.py
+++ modules/dino_encoder.py
@@ -5,7 +5,10 @@ Encode frames using DINO.
 import argparse
 
 import cv2
+import numpy as np
 import torch
+
+from video_read import ScaledReader
 
 DINO = torch.hub.load("facebookresearch/dinov2", "dinov2_vits14_reg")
 DINO.eval()
@@ -38,14 +41,45 @@ def encode_image(img, num_hidden):
     return features
 
 
-if __name__ == "__main__":
-    from vis import *
+def visualize_pca(image):
+    """
+    Returns 3 axis RGB PCA visualization.
 
+    image: [C, H, W] tensor
+    return: [H, W, C] cv2 format.
+    """
+    # Sample a subset of vectors for performance.
+    vectors = image.view(image.shape[0], -1).T
+    indices = torch.randperm(vectors.shape[0])[:1000]
+    sample = vectors[indices]
+
+    # Run PCA
+    sample_mean = sample.mean(dim=0)
+    sample_centered = sample - sample_mean
+    cov = sample_centered.T @ sample_centered / (sample_centered.shape[0] - 1)
+    eigvals, eigvecs = torch.linalg.eigh(cov)
+    top_eigvecs = eigvecs[:, -3:]
+
+    # Project all vectors onto the top 3 eigenvectors.
+    projected = (vectors - sample_mean) @ top_eigvecs
+    projected = projected - projected.min(dim=0).values
+    projected = projected / projected.max(dim=0).values
+    projected = projected.T.view(3, image.shape[1], image.shape[2])
+
+    # Convert to cv2 format.
+    projected = projected.permute(1, 2, 0).cpu().numpy()
+    projected = (projected * 255).astype(np.uint8)
+    return projected
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input")
     args = parser.parse_args()
 
-    img = read_frame(args.input)
+    cap = ScaledReader(args.input)
+    ret, img = cap.read()
+    assert ret
 
     img = resize_mult14(img)
     features = encode_image(img, 1)
@@ -55,5 +89,7 @@ if __name__ == "__main__":
 
     features = features[0]
     pca_vis = visualize_pca(features)
+    # Resize up
+    pca_vis = cv2.resize(pca_vis, (img.shape[1], img.shape[0]), interpolation=cv2.INTER_NEAREST)
     cv2.imshow("a", pca_vis)
     cv2.waitKey(0)
--- modules/video_read.py
+++ modules/video_read.py
@@ -1,22 +1,38 @@
 """
-Video reader with automatic FPS and res scaling.
-
-Note that FPS scaling will not necessarily be exact.
-I.e. 60fps / 8fps = 7.5, so every 7th or 8th frame will be used.
+Video reading and pre-processing utils.
 """
 
 import cv2
+import numpy as np
 
 from constants import *
 
 
 class ScaledReader:
-    def __init__(self, path, fps=FPS, res=RES):
+    """
+    Video reader with:
+    - Automatic FPS and res scaling.
+    - Distance warp correction.
+
+    Note that FPS scaling will not necessarily be exact.
+    I.e. 60fps / 8fps = 7.5, so every 7th or 8th frame will be used.
+
+    To correct for perspective,
+    shrink the lower edge of the image (while keeping upper fixed).
+    I.e. make all people appear around the same size.
+
+    Warping is done before resizing.
+    """
+
+    def __init__(self, path, fps=FPS, res=RES, warp_fac=0.5):
         """
         fps, res: Target FPS and res.
+        warp_fac: Shrink lower edge length by this factor.
+            0 = no warp.
         """
         self.cap = cv2.VideoCapture(path)
         self.orig_fps = self.cap.get(cv2.CAP_PROP_FPS)
+        self.orig_res = (int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
         self.new_fps = fps
         self.new_res = res
 
@@ -26,6 +42,24 @@ class ScaledReader:
 
         # Last frame read.
         self.last_frame = None
+
+        # Warp.
+        self.warp_fac = warp_fac
+        lower_length = self.orig_res[0] * (1 - self.warp_fac)
+        rect1 = np.array([
+            [0, 0],
+            [self.orig_res[0], 0],
+            [self.orig_res[0], self.orig_res[1]],
+            [0, self.orig_res[1]],
+        ], dtype=np.float32)
+        rect2 = np.array([
+            [0, 0],
+            [self.orig_res[0], 0],
+            [self.orig_res[0] / 2 + lower_length / 2, self.orig_res[1]],
+            [self.orig_res[0] / 2 - lower_length / 2, self.orig_res[1]],
+        ], dtype=np.float32)
+        self.warp_mat = cv2.getPerspectiveTransform(rect1, rect2)
+        self.inverse_warp_mat = cv2.getPerspectiveTransform(rect2, rect1)
 
     def read(self):
         """
@@ -41,6 +75,7 @@ class ScaledReader:
                 return False, None
             self.orig_frame += 1
 
+        # Read until target.
         while self.orig_frame + 0.5 < target_frame:
             ret, self.last_frame = self.cap.read()
             if not ret:
@@ -49,6 +84,9 @@ class ScaledReader:
 
         frame = cv2.resize(self.last_frame, self.new_res)
         return True, frame
+
+    def apply_warp(self, frame):
+        return cv2.warpPerspective(frame, self.warp_mat, self.orig_res)
 
 
 if __name__ == "__main__":
