7bf26f788eabeeb9f3b4a239679798dcfd153ef6
Patrick Huang <huangpatrick16777216@gmail.com>
2026-05-25 11:17:38 -0500

    Thresholding for different passes.

--- src/bounding_box.py
+++ src/bounding_box.py
@@ -5,6 +5,7 @@ Calculate the bounding box, given the CV pipeline outputs.
 import torch
 
 from pipeline import CVPipeline
+from constants import *
 from utils import *
 
 
@@ -17,20 +18,32 @@ class StaticBBox:
 
     This *does not* consider bounding box smoothness over time.
     This *does* consider results from past frames.
+
+    DINO: Static thresholding.
+    OF, BGR: Dynamic threshold:
+        Images are normalized to 0-1 every frame, and compared to a fixed thres.
+        Therefore, when there are a small amount of clear tracks, those will be highlighted.
+        When not many strong tracks, much of the space will be highlighted.
     """
 
     def __init__(self):
         # In addition to returning a value, the most recent results are kept here.
         # For visualization purposes.
-        # sim: Similarity map from DINO, lerped. torch format.
         # bbox: Bounding box coordinates. (x1, y1, x2, y2)
+        # dino: Thresholded DINO. torch format
+        # motion: OF and BGR multiplied. torch format
         self.output = {
-            "sim": None,
             "bbox": None,
+            "dino": None,
+            "motion": None,
         }
 
         self.person_embed = torch.load("person.pt")
         self.person_embed /= self.person_embed.norm()
+
+    def dynamic_thres(self, img, thres):
+        img = (img - img.min()) / (img.max() - img.min() + 1e-5)
+        return (img > thres).float()
 
     def update(self, pipe: CVPipeline):
         """
@@ -40,8 +53,25 @@ class StaticBBox:
         dino = pipe.output["dino"][0]
         if dino is not None:
             sim = cos_similarity(dino, self.person_embed)
-            sim = lerp(sim, 0.3, 1, 0, 1, clamp=True)
-            self.output["sim"] = sim
+            # Blur
+            sim = sim.unsqueeze(0).unsqueeze(0)
+            sim = torch.nn.functional.avg_pool2d(sim, kernel_size=3, stride=1, padding=1)
+            sim = sim.squeeze(0).squeeze(0)
+
+            sim = (sim > DINO_THRES).float()
+            self.output["dino"] = sim
+
+        of = pipe.output["of"]
+        bgr = pipe.output["bgr"]
+        if of is not None and bgr is not None:
+            of = torch.norm(of, dim=0)
+            of = self.dynamic_thres(of, OF_THRES)
+            bgr = bgr[0]
+            bgr = self.dynamic_thres(bgr, BGR_THRES)
+
+            #motion = of * bgr
+            motion = of
+            self.output["motion"] = motion
 
 
 def vis_static_bbox(bbox: StaticBBox, frame):
@@ -54,11 +84,16 @@ def vis_static_bbox(bbox: StaticBBox, frame):
     # Make an overlay image based on CV output. HSV.
     overlay = np.zeros_like(frame)
 
-    sim = bbox.output["sim"]
+    sim = bbox.output["dino"]
     if sim is not None:
         sim = torch_to_cv2(sim)
         sim = cv2.resize(sim, (frame.shape[1], frame.shape[0]), interpolation=cv2.INTER_NEAREST)
-        overlay[..., 2] = sim
+        overlay[sim > 0, 2] = 64
+
+    motion = bbox.output["motion"]
+    if motion is not None:
+        motion = torch_to_cv2(motion)[..., 0]
+        overlay[motion > 0, 1] = 255
 
     # Composite.
     overlay = cv2.cvtColor(overlay, cv2.COLOR_HSV2BGR).astype(float)
--- src/constants.py
+++ src/constants.py
@@ -11,4 +11,8 @@ DINO_INTERVAL = 5
 # Bottom edge is this factor of original size. 1 means no warp.
 WARP_CORRECTION = 0.5
 
+DINO_THRES = 0.2
+OF_THRES = 0.3
+BGR_THRES = 0
+
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
